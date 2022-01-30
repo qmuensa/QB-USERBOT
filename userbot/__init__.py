415375da5b13bbd402e7a7376225640ff5262de6
dnrbaihaqi <97588366+qmuensa@users.noreply.github.com>
2022-01-30 13:09:19 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -149,7 +149,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.'com/dnrbaihaqi/QB-USERBOT.git"
+    "UPSTREAM_REPO_URL", "https://github.'com/qmuensa/QB-USERBOT.git"
 )
 
 # SQL Database URI
